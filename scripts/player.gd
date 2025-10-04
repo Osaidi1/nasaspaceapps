de99362e6c1431ef2a66ae2a7d86c9c482919211
Mustafa Hassan <mustafaazeem@hotmail.com>
2025-10-04 14:44:13 +0200

    character done

--- scripts/player.gd
+++ scripts/player.gd
@@ -2,15 +2,16 @@ extends CharacterBody2D
 
 @onready var animated: AnimatedSprite2D = $AnimatedSprite2D
 
-var is_moving = false
 var velo_dir: Vector2
 var velo: Vector2
 const SPEED = 100
+var is_moving = false
 
 func _physics_process(_delta: float) -> void:
 	#Get Input
 	velo = Vector2(Input.get_axis("left", "right"), Input.get_axis("up", "down"))
-	if is_moving:
+	if velo.x != 0 or velo.y != 0:
+
 		velo_dir = velo
 	if velo.length() > 0:
 		velo = velo.normalized()
@@ -18,7 +19,8 @@ func _physics_process(_delta: float) -> void:
 	velocity.x = velo.x * SPEED
 	velocity.y = velo.y * SPEED
 	
-	if Input.is_action_pressed("left") or Input.is_action_pressed("right") or Input.is_action_pressed("up") or Input.is_action_pressed("down"):
+	#Set Moving var
+	if velo.x != 0 or velo.y != 0:
 		is_moving = true
 	else:
 		is_moving = false
@@ -34,9 +36,8 @@ func _physics_process(_delta: float) -> void:
 	velo.y = 0
 
 func anims():
-	
-	if Input.is_action_pressed("left") or Input.is_action_pressed("right") or Input.is_action_pressed("down") or Input.is_action_pressed("up"):
-		if is_moving:
+	if is_moving:
+		if Input.is_action_pressed("left") or Input.is_action_pressed("right") or Input.is_action_pressed("down") or Input.is_action_pressed("up"):
 			if velo_dir.x > 0:
 				if velo_dir.y > 0:
 					animated.play("dr-walk")
@@ -44,18 +45,18 @@ func anims():
 					animated.play("ur-walk")
 				else:
 					animated.play("r-walk")
-		#Left
-		elif velo_dir.x < 0:
-			if velo_dir.y > 0:
-				animated.play("dl-walk")
+			#Left
+			elif velo_dir.x < 0:
+				if velo_dir.y > 0:
+					animated.play("dl-walk")
+				elif velo_dir.y < 0:
+					animated.play("ul-walk")
+				else:
+					animated.play("l-walk")
 			elif velo_dir.y < 0:
-				animated.play("ul-walk")
-			else:
-				animated.play("l-walk")
-		elif velo_dir.y < 0:
-			animated.play("u-walk")
-		elif velo_dir.y > 0:
-			animated.play("d-walk")
+				animated.play("u-walk")
+			elif velo_dir.y > 0:
+				animated.play("d-walk")
 	else:
 		#Right
 		if velo_dir.x > 0:
